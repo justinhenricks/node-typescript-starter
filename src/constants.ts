da5cd06f06bde6aae50a6806b62bcb5a46ee76a0
Justin Henricks <justinhenricks@gmail.com>
2024-05-11 15:06:28 -0400

    init express

---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const IS_PROD = process.env.NODE_ENV === "production";
+export const PORT = process.env.PORT || 3000;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("hello world justin :)");
+import "dotenv/config";
+import express from "express";
+import { IS_PROD, PORT } from "./constants";
+
+async function main() {
+  const app = express();
+
+  app.use(express.json());
+  // app.use(router); TODO
+
+  app.listen(PORT, () => {
+    console.log(
+      `🚀 Server is running on port: ${PORT} and in ${
+        IS_PROD ? "production" : "development."
+      }`
+    );
+  });
+}
+
+main();
