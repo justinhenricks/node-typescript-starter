296f8211251bf63154c15df8d9e2434e7717077e
Justin Henricks <justinhenricks@gmail.com>
2024-05-11 19:55:23 -0400

    light error middleware..

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,23 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  res.send("🚀 Hello World!");
+router.get("/", async (req, res, next) => {
+  try {
+    res.send("🚀 Hello World!");
+  } catch (error) {
+    return next(error);
+  }
 });
 
+router.use((err: unknown, req: express.Request, res: express.Response) => {
+  if (err instanceof Error) {
+    const errorResponse = { message: err.message };
+
+    res.status(400).json(errorResponse);
+
+    return;
+  }
+
+  res.status(500).json({ message: "Internal Server Error" });
+});
 export { router };
